b72af0be6002381ce22c47750d182d3722d9716a
Michal <matej50@gmail.com>
2020-10-31 22:03:13 +0100

    first tests

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './App.css';
-import TodoContext from './TodoContext';
+import './style/App.css';
+import TodoContext from './contexts/TodoContext';
 
 ReactDOM.render(
   <React.StrictMode>
---
+++ src/tests/App.test.js
@@ -0,0 +1,17 @@
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { shallow } from 'enzyme';
+import TodoContext from '../contexts/TodoContext';
+import TodoList from '../components/TodoList';
+
+it('renders without crashing', () => {
+  shallow(<TodoContext />);
+});
+it('includes input', () => {
+  const app = shallow(<TodoContext />);
+  expect(app.containsMatchingElement(<input />)).toEqual(true)
+});
+it('includes TodoList', () => {
+  const app = shallow(<TodoContext />);
+  expect(app.containsMatchingElement(<TodoList />)).toEqual(true)
+});

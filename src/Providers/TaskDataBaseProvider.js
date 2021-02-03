a20dc8e610523cf9d36f0f8b9cdd6cb039093834
Michal <matej50@gmail.com>
2021-02-03 11:44:34 +0100

    added form to new category

--- src/Providers/TaskDataBaseProvider.js
+++ src/Providers/TaskDataBaseProvider.js
@@ -1,33 +1,42 @@
-export default class TaskDataBaseProvider {
+import { render } from "@testing-library/react";
+import React, { Component } from 'react'
+import CategoryList from "../components/CategoryList";
+
+export default class TaskDataBaseProvider extends Component {
     tasks = null;
-    arr = [{"category":"default"}]
-    constructor(){
+    arr = [{ "category": "default" }];
+    constructor(props) {
+        super(props);
         this.LoadData();
-        if(this.tasks === null)
-        {
+        if (this.tasks === null) {
             localStorage.setItem('tasks', JSON.stringify(this.arr))
         }
+        let categoryList = new CategoryList();
+        this.state = {tasks:  categoryList.state.tasks}
     }
-    LoadData(){
+
+    LoadData() {
         this.tasks = JSON.parse(localStorage.getItem('tasks'))
     }
 
     getCategories() {
-        
+
         return [...new Set(this.tasks.map(item => {
             console.log(item.category)
             return item.category;
-        }))] 
+        }))]
     }
-        
-        
-    
 
-    getTasks(){
+    getTasks() {
         return [...new Set(this.tasks.map(item => {
             console.log(item.text)
             return item.text;
         }))]
     }
 
+    
+    addCategory() {
+        localStorage.setItem('tasks', JSON.stringify(this.state.tasks))
+    
+    }
 }
--- src/components/CategoryList.js
+++ src/components/CategoryList.js
@@ -3,9 +3,38 @@ import TaskDataBaseProvider from '../Providers/TaskDataBaseProvider'
 export default class CategoryList extends Component {
     constructor(props) {
         super(props);
-        let taskProvider = new TaskDataBaseProvider();
-        this.state = { todos: taskProvider.getCategories() };
+        let categoryProvider = new TaskDataBaseProvider();
+        this.state = { todos: categoryProvider.getCategories(), tasks: [], category: "", local: categoryProvider.addCategory() };
     }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        const newTask = {
+            //   text: this.state.text,
+            //   date: this.state.date,
+            category: this.state.category,
+            //   id: Date.now()
+        };
+        this.setState(state => ({
+            tasks: state.tasks.concat(newTask),
+            //   text: '',
+            //   displayCategory: this.state.category
+
+        }));
+
+    }
+
+    categoryChange = (e) => {
+        this.setState({ category: e.target.value })
+
+        console.log(this.state.category)
+    }
+
+    componentDidUpdate(){
+        this.state.local();
+    }
+
+
     render() {
         return (
             <div>
@@ -17,6 +46,12 @@ export default class CategoryList extends Component {
                             {category}
                         </button>
                     ))}
+                </div>
+                <div className="newCategory">
+                    <form onSubmit={this.handleSubmit} className="add-category">
+                        <input onChange={this.categoryChange} type="text" value={this.state.category} placeholder="Enter the category" />
+                        <button type="submit"> + </button>
+                    </form>
                 </div>
             </div>
         )
